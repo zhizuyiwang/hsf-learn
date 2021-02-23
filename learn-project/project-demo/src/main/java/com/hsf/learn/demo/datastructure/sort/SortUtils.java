5753d82a512741d767c3f5624cfb88120f0a9ac7
huangshanfei <1175432154@qq.com>
2021-02-23 17:25:52 +0800

    冒泡排序

--- learn-project/project-demo/src/main/java/com/hsf/learn/demo/datastructure/sort/SortUtils.java
+++ learn-project/project-demo/src/main/java/com/hsf/learn/demo/datastructure/sort/SortUtils.java
@@ -127,11 +127,19 @@ public class SortUtils {
     /**
      * 六、交换排序之冒泡排序
      *
-     *
      * @param array
      * @return
      */
     public static int[] bubbleSort(int[] array){
+        for(int i = 0; i < array.length - 1; i++){
+            for (int j = 0; j < array.length - i - 1; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
 
         return array;
     }
